84026250033f523be049343366829b0a84f2978d
adikampli <adikampli@gmail.com>
2014-09-08 16:45:02 +0530

    Added null checks

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,3 @@
-
-
 var app = require('../../server/server');
 var ds = app.dataSources.db;
 var products = ds.getModel ('product');
@@ -47,106 +45,117 @@ module.exports = function(product){
         var productHandler = app.models.productHandler;
         productHandler.findProductById(id ,function(err, productRes){
             var productView = productRes.CatalogEntryView;
-            productView.forEach(function(product){
-                productObj.identifier = product.uniqueID;
-                productObj.name = product.name;
-                productObj.shortDesc = product.shortDescription;
-                productObj.longDesc = product.longDescription;
-                productObj.parentCategoryId = [product.parentCategoryID];
-                productObj.partNumber = product.partNumber;
-                productObj.productType = product.productType;
-                productObj.metaKeyword = product.metaKeyword;
-                productObj.metaDesc = product.metaDescription;
-                productObj.title = product.title;
-                productObj.thumbnail = product.thumbnail;
-                productObj.fullImage = product.fullImage;
-                productObj.altImageText = product.fullImageAltDescription;
-                // productObj.inventory = "dummy inventory";
-                productObj.numberOfSKU = product.numberOfSKUs;
+            if(productView){
+                productView.forEach(function(product){
+                    productObj.identifier = product.uniqueID;
+                    productObj.name = product.name;
+                    productObj.shortDesc = product.shortDescription;
+                    productObj.longDesc = product.longDescription;
+                    productObj.parentCategoryId = [product.parentCategoryID];
+                    productObj.partNumber = product.partNumber;
+                    productObj.productType = product.productType;
+                    productObj.metaKeyword = product.metaKeyword;
+                    productObj.metaDesc = product.metaDescription;
+                    productObj.title = product.title;
+                    productObj.thumbnail = product.thumbnail;
+                    productObj.fullImage = product.fullImage;
+                    productObj.altImageText = product.fullImageAltDescription;
+                    // productObj.inventory = "dummy inventory";
+                    productObj.numberOfSKU = product.numberOfSKUs;
 
-                var prodPrices = product.Price;
-                prodPrices.forEach(function(prodPrice){
-                    priceObj.unitPrice = prodPrice.priceValue;
-                    /*priceObj.productId = product.uniqueID;
-                    priceObj.partNumber = product.partNumber;
-                    priceObj.resourceId = productRes.resourceId;
-                    priceObj.resourceName = productRes.resourceName;*/
-                    priceObj.priceDesc = prodPrice.priceDescription;
-                    priceObj.priceUsage = prodPrice.priceUsage;
-                    priceObj.priceValue = prodPrice.priceValue;
-                });
-                
-                productObj.Price = priceObj;
-
-                /*skuAttachmentsObj.mimeType = "dummy attachment mimeType";
-                skuAttachmentsObj.path = "dummy attachment path";
-                skuAttachmentsObj.identifier = "dummy attachment identifier";
-                skuAttachmentsObj.metadata = ["dummy attachment metadata"];
-*/
-                var skus = product.SKUs;
-                skus.forEach(function(sku){
-                    var skusAttrs = sku.Attributes;
-                    var skuAttrs = [];
-                    skusAttrs.forEach(function(skuAttr){
-                        var skuAttributesObj = new skuAttributesClass();
-                        var skuValuesObj = new skuValuesClass();
-                        var skuAttrVals = skuAttr.Values;
-                        skuAttrVals.forEach(function(skuAttrVal){
-                            skuValuesObj.values = [skuAttrVal.values];
-                            skuAttributesObj.identifier = skuAttrVal.identifier;
+                    var prodPrices = product.Price;
+                    if(prodPrices){
+                        prodPrices.forEach(function(prodPrice){
+                            priceObj.unitPrice = prodPrice.priceValue;
+                            /*priceObj.productId = product.uniqueID;
+                            priceObj.partNumber = product.partNumber;
+                            priceObj.resourceId = productRes.resourceId;
+                            priceObj.resourceName = productRes.resourceName;*/
+                            priceObj.priceDesc = prodPrice.priceDescription;
+                            priceObj.priceUsage = prodPrice.priceUsage;
+                            priceObj.priceValue = prodPrice.priceValue;
                         });
-                        
-                        // skuValuesObj.metadata = ["dummy value metadata"];
-                        skuAttributesObj.values = skuValuesObj;
-                        skuAttributesObj.comparable = skuAttr.comparable;
-                        skuAttributesObj.searchable = skuAttr.searchable;
-                        skuAttributesObj.displayable = skuAttr.displayable;
-                        // skuAttributesObj.metadata = ["dummy metadata"];
-                        skuAttrs.push(skuAttributesObj);
-                    });
+                    }
                     
-                    skuObj.identifier = sku.SKUUniqueID;
-                    var skusPrices = sku.Price;
-                    skusPrices.forEach(function(skuPrice){
-                        skuObj.price = skusPrices.SKUPriceValue;    
-                    });
-                    // skuObj.attachments = skuAttachmentsObj;
-                    skuObj.attributes = skuAttrs;
-                    // skuObj.metadata = ["dummy metadata"];
+                    productObj.Price = priceObj;
+
+                    /*skuAttachmentsObj.mimeType = "dummy attachment mimeType";
+                    skuAttachmentsObj.path = "dummy attachment path";
+                    skuAttachmentsObj.identifier = "dummy attachment identifier";
+                    skuAttachmentsObj.metadata = ["dummy attachment metadata"];
+    */
+                    var skus = product.SKUs;
+                    if(skus){
+                        skus.forEach(function(sku){
+                            var skusAttrs = sku.Attributes;
+                            var skuAttrs = [];
+                            if(skusAttrs){
+                                skusAttrs.forEach(function(skuAttr){
+                                    var skuAttributesObj = new skuAttributesClass();
+                                    var skuValuesObj = new skuValuesClass();
+                                    var skuAttrVals = skuAttr.Values;
+                                    if(skuAttrVals){
+                                        skuAttrVals.forEach(function(skuAttrVal){
+                                            skuValuesObj.values = [skuAttrVal.values];
+                                            skuAttributesObj.identifier = skuAttrVal.identifier;
+                                        });
+                                    }
+                                    
+                                    // skuValuesObj.metadata = ["dummy value metadata"];
+                                    skuAttributesObj.values = skuValuesObj;
+                                    skuAttributesObj.comparable = skuAttr.comparable;
+                                    skuAttributesObj.searchable = skuAttr.searchable;
+                                    skuAttributesObj.displayable = skuAttr.displayable;
+                                    // skuAttributesObj.metadata = ["dummy metadata"];
+                                    skuAttrs.push(skuAttributesObj);
+                                });
+                            }
+                            skuObj.identifier = sku.SKUUniqueID;
+                            var skusPrices = sku.Price;
+                            if(skusPrices){
+                                skusPrices.forEach(function(skuPrice){
+                                    skuObj.price = skusPrices.SKUPriceValue;    
+                                });
+                            }
+                            // skuObj.attachments = skuAttachmentsObj;
+                            skuObj.attributes = skuAttrs;
+                            // skuObj.metadata = ["dummy metadata"];
+                        });
+                    
+                        productObj.Sku = skuObj;
+                    }
+                    /*attachmentsObj.mimeType = "dummy attachment mimeType";
+                    attachmentsObj.path = "dummy attachment path";
+                    attachmentsObj.identifier = "dummy attachment identifier";
+                    attachmentsObj.metadata = ["dummy attachment metadata"];
+                    productObj.Attachments = attachmentsObj;*/
+                    
+                    var attributes = product.Attributes;
+                    var attrObj = [];
+                    if(attributes){
+                        attributes.forEach(function(attribute){
+                            var attributesObj = new attributesClass();
+                            // var attrValuesObj = new attrValuesClass();
+                            attributesObj.identifier = attribute.identifier;
+
+                            // attrValuesObj.values = ["dummy values"];
+                            // attrValuesObj.metadata = ["dummy values"];
+                            // attributesObj.values = attrValuesObj;
+
+                            attributesObj.comparable = attribute.comparable;
+                            attributesObj.searchable = attribute.searchable;
+                            attributesObj.displayable = attribute.displayable;
+                            // attributesObj.metadata = ["dummy metadata"];
+                            attrObj.push(attributesObj);
+                        });
+                    }
+                    productObj.Attributes = attrObj;
+
+                    /*valuesObj.values = ["dummy values"];
+                    valuesObj.metadata = ["dummy value metadata"];
+                    productObj.Values = valuesObj;*/
                 });
-                
-                productObj.Sku = skuObj;
-
-                /*attachmentsObj.mimeType = "dummy attachment mimeType";
-                attachmentsObj.path = "dummy attachment path";
-                attachmentsObj.identifier = "dummy attachment identifier";
-                attachmentsObj.metadata = ["dummy attachment metadata"];
-                productObj.Attachments = attachmentsObj;*/
-                
-                var attributes = product.Attributes;
-                var attrObj = [];
-                attributes.forEach(function(attribute){
-                    var attributesObj = new attributesClass();
-                    // var attrValuesObj = new attrValuesClass();
-                    attributesObj.identifier = attribute.identifier;
-
-                    // attrValuesObj.values = ["dummy values"];
-                    // attrValuesObj.metadata = ["dummy values"];
-                    // attributesObj.values = attrValuesObj;
-
-                    attributesObj.comparable = attribute.comparable;
-                    attributesObj.searchable = attribute.searchable;
-                    attributesObj.displayable = attribute.displayable;
-                    // attributesObj.metadata = ["dummy metadata"];
-                    attrObj.push(attributesObj);
-                });
-                productObj.Attributes = attrObj;
-
-                /*valuesObj.values = ["dummy values"];
-                valuesObj.metadata = ["dummy value metadata"];
-                productObj.Values = valuesObj;*/
-            });
-            
+            }
             cb(null, productObj);
         });
     }
@@ -185,28 +194,30 @@ module.exports = function(product){
             var productView = searchRes.catalogEntryView;
             // console.log(productView.length);
             var searchArray = [];
-            productView.forEach(function(product){
-                var productObj = new productClass();
-                productObj.identifier = product.uniqueID;
-                productObj.name = product.name;
-                productObj.shortDesc = product.shortDescription;
-                productObj.parentCategoryId = [product.parentCatalogGroupID];
-                productObj.partNumber = product.partNumber;
-                productObj.thumbnail = product.thumbnail;
-                productObj.altImageText = product.fullImageAltDescription;
+            if(productView){
+                productView.forEach(function(product){
+                    var productObj = new productClass();
+                    productObj.identifier = product.uniqueID;
+                    productObj.name = product.name;
+                    productObj.shortDesc = product.shortDescription;
+                    productObj.parentCategoryId = [product.parentCatalogGroupID];
+                    productObj.partNumber = product.partNumber;
+                    productObj.thumbnail = product.thumbnail;
+                    productObj.altImageText = product.fullImageAltDescription;
 
-                var prodPrices = product.price;
-                prodPrices.forEach(function(prodPrice){
-                    priceObj.priceDesc = prodPrice.description;
-                    priceObj.priceUsage = prodPrice.usage;
-                    priceObj.priceValue = prodPrice.value;
+                    var prodPrices = product.price;
+                    if(prodPrices){
+                        prodPrices.forEach(function(prodPrice){
+                            priceObj.priceDesc = prodPrice.description;
+                            priceObj.priceUsage = prodPrice.usage;
+                            priceObj.priceValue = prodPrice.value;
+                        });
+                    }
+                    productObj.Price = priceObj;
+
+                    searchArray.push(productObj);
                 });
-                
-                productObj.Price = priceObj;
-
-                searchArray.push(productObj);
-            });
-            
+            }
             cb(null, searchArray);
         });
     }
