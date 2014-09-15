23990ea9d3018c755213afc6d914d81a3a36dead
SurojitMandal <job4surojit@gmail.com>
2014-09-15 15:26:56 +0530

    Display cart function is added

--- common/models/order-handler.js
+++ common/models/order-handler.js
@@ -29,8 +29,8 @@ module.exports = function(OrderHandler){
     );
 	//Add item 2 cart:
 	OrderHandler.addItem2Cart = function(prodId, qty, wcToken, trustedToken, pId, uId, cb) {
-        var uri = 'https://toothless.ngrok.com/wcs/resources/store/10151/cart';
-        console.log(uri); 
+        var uri = 'https://localhost/wcs/resources/store/10151/cart';
+        //console.log("prodId: "+prodId+" QTY: "+qty); 
         request({
             url: uri,
             method: 'POST',
@@ -42,26 +42,27 @@ module.exports = function(OrderHandler){
 			json:{
 				"orderItem": [{
 				"productId": prodId,
-				"quantity": qty
+				"quantity": ''+qty
 				}]
 			},
         }, 
 		function(err, response) {
             if (err) console.error(err);
-			console.log('Result: '+JSON.stringify(response.body));
-			cb(null, response.body);
+			//console.log('Result: '+JSON.stringify(response.body));
+			cb(null, response);
         });	
     }
     OrderHandler.remoteMethod(
         'addItem2Cart', 
         {
-          accepts: [{arg: 'prodId', type: 'string'},{arg: 'qty', type: 'string'},{arg: 'wcToken', type: 'string'},
+          accepts: [{arg: 'prodId', type: 'string'},{arg: 'qty', type: 'number'},{arg: 'wcToken', type: 'string'},
 					{arg: 'trustedToken', type: 'string'},{arg: 'pId', type: 'string'},{arg: 'uid', type: 'string'}],
           returns: {arg: 'add2Cart', type: 'string'},
         }
     );
 
 	//Display cart:
+	/*
 	OrderHandler.displayCart = function(displayCart,  cb) {
 	
         var uri = 'http://restfalcon.mybluemix.net/displayCart/9';
@@ -76,13 +77,32 @@ module.exports = function(OrderHandler){
         });	
 		
 	 
+    }*/
+    
+		OrderHandler.displayCart = function( wcToken, trustedToken, pId, uId, cb) {
+        var uri = 'https://localhost/wcs/resources/store/10151/cart/@self';
+
+        request({
+            url: uri,
+            method: 'GET',
+			headers:{"WCToken": wcToken,
+					"WCTrustedToken": trustedToken,
+					"personalizationID": pId,
+					"userId": uId
+			}
+        }, 
+		function(err, response) {
+            if (err) console.error(err);
+			console.log('Result: '+JSON.stringify(response.body));
+			cb(null, '{'+response.request.headers+","+response.body+'}');
+        });	
     }
-     
+	
     OrderHandler.remoteMethod(
         'displayCart', 
         {
-          accepts: [{arg: 'reqBody', type: 'string'}],
-          returns: {arg: 'displayCart', type: 'string'},
+          accepts: [{arg: 'wcToken', type: 'string'},{arg: 'trustedToken', type: 'string'},{arg: 'pId', type: 'string'},{arg: 'uid', type: 'string'}],
+          returns: {arg: 'cartresponse', type: 'string'},
         }
     );
 	
--- common/models/order.js
+++ common/models/order.js
@@ -1,15 +1,76 @@
 var app = require('../../server/server');
 var ds = app.dataSources.db;
-var order = ds.getModel ('order');
+var ord = ds.getModel ('order');
         
 // Hiding the existing remote methods.
-order.sharedClass.find('create',true).shared = false;
-order.sharedClass.find('upsert',true).shared = false;
-order.sharedClass.find('exists',true).shared = false;
-order.sharedClass.find('findById',true).shared = false;
-order.sharedClass.find('find',true).shared = false;
-order.sharedClass.find('findOne',true).shared = false;
-order.sharedClass.find('updateAll',true).shared = false;
-order.sharedClass.find('deleteById',true).shared = false;
-order.sharedClass.find('count',true).shared = false;
-order.sharedClass.find('updateAttributes',false).shared = false;
+ord.sharedClass.find('create',true).shared = false;
+ord.sharedClass.find('upsert',true).shared = false;
+ord.sharedClass.find('exists',true).shared = false;
+ord.sharedClass.find('findById',true).shared = false;
+ord.sharedClass.find('find',true).shared = false;
+ord.sharedClass.find('findOne',true).shared = false;
+ord.sharedClass.find('updateAll',true).shared = false;
+ord.sharedClass.find('deleteById',true).shared = false;
+ord.sharedClass.find('count',true).shared = false;
+ord.sharedClass.find('updateAttributes',false).shared = false;
+
+module.exports = function(order){
+	//add item to cart:
+	order.addItemToCart = function(productId, qty, cb) {
+		var wcToken ; 
+		var wcTrustedToken;
+		var uId;
+		var pId;
+
+		var liHandlers = app.models.LoginIdentityHandler;
+			liHandlers.guestIdentity( function(err, liHandlersRes){
+				console.log('Guest Id response::'+liHandlersRes);
+				if(liHandlersRes){
+					var wcToken = liHandlersRes.WCToken; 
+					var wcTrustedToken  = liHandlersRes.WCTrustedToken;
+					var pId = liHandlersRes.personalizationID;
+					var uId = liHandlersRes.userId;
+					var ordHandlers = app.models.OrderHandler;
+					
+					ordHandlers.addItem2Cart(productId, qty, wcToken, wcTrustedToken, pId, uId, function(err, ordHandlersRes){
+						//console.log(ordHandlersRes);
+
+						cb(null, ordHandlersRes);
+					});
+				}		
+			});
+	}
+    order.remoteMethod(
+        'addItemToCart', 
+        {
+          accepts: [{arg: 'productId', type: 'string'},{arg: 'qty', type: 'number'}],
+		  http: {path: '/addItemToCart', verb: 'post'},
+          returns: {arg: 'addToCart', type: 'JSON'},
+        }
+    );
+	
+	
+	
+	//Get cart details:
+	order.getCartDetails = function( wcToken, trustedToken, pId, uId, cb) {
+
+		var ordHandlers = app.models.OrderHandler;
+		
+		ordHandlers.displayCart(wcToken, trustedToken, pId, uId, function(err, cartResp){
+			console.log(cartResp);
+			if(cartResp){
+				console.log('Cart response:::: '+cartResp);
+			}
+				cb(null, cartResp);
+		});
+
+	}
+    order.remoteMethod(
+        'getCartDetails', 
+        {
+          accepts: [{arg: 'wcToken', type: 'string'},{arg: 'trustedToken', type: 'string'},{arg: 'pId', type: 'string'},{arg: 'uid', type: 'string'}],
+		  http: {path: '/getCartDetails', verb: 'post'},
+          returns: {arg: 'CartResp', type: 'JSON'},
+        }
+    );	
+};
